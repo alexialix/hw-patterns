c6f08e1adbbc0d4da647caae7a92c8b9224e2367
Alexia <trup.lexa@gmail.com>
2024-12-17 11:55:27 +0500

    Initial commit

---
+++ src/main/java/ru/netology/DataGenerator.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DataGenerator {
+    private static final Faker faker = new Faker(new Locale("ru"));  // Создаем один экземпляр Faker для всех методов
+
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{"Москва", "Санкт-Петербург", "Казань", "Нижний Новгород", "Владивосток"};
+        return cities[ThreadLocalRandom.current().nextInt(cities.length)];
+    }
+
+    public static String generateName() {
+        return faker.name().fullName();  // Используем один экземпляр Faker
+    }
+
+    public static String generatePhone() {
+        return faker.phoneNumber().phoneNumber();  // Используем один экземпляр Faker
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String ru) {
+            return new UserInfo(
+                    generateCity(),
+                    generateName(),
+                    generatePhone()
+            );
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
